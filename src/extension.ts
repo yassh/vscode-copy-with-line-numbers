65e06c736a181818b45d3e28720df2b4e1d5b8c0
yassh <yassh32@outlook.jp>
2017-09-11 01:09:56 +0900

    implement Copy With Line Numbers: Without Path

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,54 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+'use strict'
+import * as vscode from 'vscode'
+import { EOL } from 'os'
+import { copy } from 'copy-paste'
+import * as leftPad from 'left-pad'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const COMMAND_WITHOUT_PATH = 'extension.copyWithLineNumbers.withoutPath'
+const MULTI_SELECTION_SEPARATOR = '---'
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "copy-with-line-numbers" is now active!');
+function getLinesWithNumbers() {
+  const editor = vscode.window.activeTextEditor
+  if (!editor) return
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    // The code you place here will be executed every time your command is executed
+  const document = editor.document
+  const selections = [...editor.selections].sort((a, b) => a.start.line - b.start.line)
+  const lastSelection = selections[selections.length - 1]
+  const largestLineNumber = lastSelection.end.line + 1
+  const largestLineNumberLength = largestLineNumber.toString().length
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
+  let lines = ''
+  selections.forEach((selection, i) => {
+    if (i > 0) lines += `${MULTI_SELECTION_SEPARATOR}${EOL}`
 
-  context.subscriptions.push(disposable);
+    const startLine = selection.start.line
+    const endLine = selection.end.line
+    for (let n = startLine; n <= endLine; n += 1) {
+      const number = leftPad(n + 1, largestLineNumberLength, 0)
+      const line = document.lineAt(n).text
+
+      lines += `${number}: ${line}${EOL}`
+    }
+  })
+  return lines
 }
 
-// this method is called when your extension is deactivated
+function copyWithLineNumbers(filePath?: string) {
+  const editor = vscode.window.activeTextEditor
+  if (!editor) return
+
+  const str = (filePath ? `File: ${filePath}${EOL}` : '') + getLinesWithNumbers()
+  copy(str)
+}
+
+function withoutPath() {
+  copyWithLineNumbers()
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(COMMAND_WITHOUT_PATH, () => { withoutPath() })
+  context.subscriptions.push(disposable)
+}
+
 export function deactivate() {
 }
