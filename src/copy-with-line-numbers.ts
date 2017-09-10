cb86ad5a7650dc911032828094aa35fc6e31b9c1
yassh <yassh32@outlook.jp>
2017-09-11 02:40:35 +0900

    implement Copy With Line Numbers: With Full Path

---
+++ src/copy-with-line-numbers.ts
@@ -0,0 +1,62 @@
+'use strict'
+
+import * as vscode from 'vscode'
+import { EOL } from 'os'
+import { copy } from 'copy-paste'
+import * as leftPad from 'left-pad'
+
+const MULTI_SELECTION_SEPARATOR = '---'
+
+export const COMMAND_IDS = {
+  WITHOUT_PATH: 'extension.copyWithLineNumbers.withoutPath',
+  WITH_FULL_PATH: 'extension.copyWithLineNumbers.withFullPath',
+}
+
+function getLinesWithNumbers() {
+  const editor = vscode.window.activeTextEditor
+  const document = editor.document
+  const selections = [...editor.selections].sort((a, b) => a.start.line - b.start.line)
+  const lastSelection = selections[selections.length - 1]
+  const largestLineNumber = lastSelection.end.line + 1
+  const largestLineNumberLength = largestLineNumber.toString().length
+
+  let lines = ''
+  selections.forEach((selection, i) => {
+    if (i > 0) lines += `${MULTI_SELECTION_SEPARATOR}${EOL}`
+
+    for (let n = selection.start.line; n <= selection.end.line; n += 1) {
+      const number = leftPad(n + 1, largestLineNumberLength, 0)
+      const line = document.lineAt(n).text
+
+      lines += `${number}: ${line}${EOL}`
+    }
+  })
+  return lines
+}
+
+function copyWithLineNumbers(filePath?: string) {
+  const str = (filePath ? `File: ${filePath}${EOL}` : '') + getLinesWithNumbers()
+  copy(str)
+  vscode.window.showInformationMessage('Copied!')
+}
+
+const commands = {
+  [COMMAND_IDS.WITHOUT_PATH]: () => {
+    copyWithLineNumbers()
+  },
+  [COMMAND_IDS.WITH_FULL_PATH]: () => {
+    const fullPath = vscode.window.activeTextEditor.document.fileName
+    copyWithLineNumbers(fullPath)
+  },
+}
+
+export function runCommand(commandId) {
+  const editor = vscode.window.activeTextEditor
+  if (!editor) {
+    vscode.window.showInformationMessage('No editor is active')
+    return
+  }
+
+  const command = commands[commandId]
+  if (command) command()
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,18 @@
 'use strict'
+
 import * as vscode from 'vscode'
-import { EOL } from 'os'
-import { copy } from 'copy-paste'
-import * as leftPad from 'left-pad'
-
-const COMMAND_WITHOUT_PATH = 'extension.copyWithLineNumbers.withoutPath'
-const MULTI_SELECTION_SEPARATOR = '---'
-
-function getLinesWithNumbers() {
-  const editor = vscode.window.activeTextEditor
-  if (!editor) return
-
-  const document = editor.document
-  const selections = [...editor.selections].sort((a, b) => a.start.line - b.start.line)
-  const lastSelection = selections[selections.length - 1]
-  const largestLineNumber = lastSelection.end.line + 1
-  const largestLineNumberLength = largestLineNumber.toString().length
-
-  let lines = ''
-  selections.forEach((selection, i) => {
-    if (i > 0) lines += `${MULTI_SELECTION_SEPARATOR}${EOL}`
-
-    const startLine = selection.start.line
-    const endLine = selection.end.line
-    for (let n = startLine; n <= endLine; n += 1) {
-      const number = leftPad(n + 1, largestLineNumberLength, 0)
-      const line = document.lineAt(n).text
-
-      lines += `${number}: ${line}${EOL}`
-    }
-  })
-  return lines
-}
-
-function copyWithLineNumbers(filePath?: string) {
-  const editor = vscode.window.activeTextEditor
-  if (!editor) return
-
-  const str = (filePath ? `File: ${filePath}${EOL}` : '') + getLinesWithNumbers()
-  copy(str)
-}
-
-function withoutPath() {
-  copyWithLineNumbers()
-}
+import { COMMAND_IDS, runCommand } from './copy-with-line-numbers'
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(COMMAND_WITHOUT_PATH, () => { withoutPath() })
-  context.subscriptions.push(disposable)
+  const { registerCommand } = vscode.commands
+  const { subscriptions } = context
+  const {
+    WITHOUT_PATH,
+    WITH_FULL_PATH,
+  } = COMMAND_IDS
+
+  subscriptions.push(registerCommand(WITHOUT_PATH, () => { runCommand(WITHOUT_PATH) }))
+  subscriptions.push(registerCommand(WITH_FULL_PATH, () => { runCommand(WITH_FULL_PATH) }))
 }
 
 export function deactivate() {
