763cb0f771c2e8f2aad5e95e0f06a1c8361f8106
yassh <yassh32@outlook.jp>
2017-09-12 10:54:25 +0900

    refactor

--- src/copy-with-line-numbers.ts
+++ src/copy-with-line-numbers.ts
@@ -7,63 +7,65 @@ import { copy } from 'copy-paste'
 import * as leftPad from 'left-pad'
 
 const MULTI_SELECTION_SEPARATOR = '---'
+const COPY_WITH_LINE_NUMBERS = 'extension.copyWithLineNumbers'
+const COPY_WITH_LINE_NUMBERS_WITH_FULL_PATH = COPY_WITH_LINE_NUMBERS + '.withFullPath'
+const COPY_WITH_LINE_NUMBERS_WITH_FILE_NAME = COPY_WITH_LINE_NUMBERS + '.withFileName'
+const OPTION_WITH_FULL_PATH = 'withFullPath'
+const OPTION_WITH_FILE_NAME = 'withFileName'
 
-export const COMMAND_IDS = {
-  WITHOUT_PATH: 'extension.copyWithLineNumbers.withoutPath',
-  WITH_FULL_PATH: 'extension.copyWithLineNumbers.withFullPath',
-  WITH_FILE_NAME: 'extension.copyWithLineNumbers.withFileName',
-}
-
-function getLinesWithNumbers() {
-  const editor = vscode.window.activeTextEditor
-  const document = editor.document
-  const selections = [...editor.selections].sort((a, b) => a.start.line - b.start.line)
-  const lastSelection = selections[selections.length - 1]
-  const largestLineNumber = lastSelection.end.line + 1
-  const largestLineNumberLength = largestLineNumber.toString().length
-
-  let lines = ''
-  selections.forEach((selection, i) => {
-    if (i > 0) lines += `${MULTI_SELECTION_SEPARATOR}${EOL}`
-
-    for (let n = selection.start.line; n <= selection.end.line; n += 1) {
-      const number = leftPad(n + 1, largestLineNumberLength, 0)
-      const line = document.lineAt(n).text
-
-      lines += `${number}: ${line}${EOL}`
-    }
-  })
-  return lines
-}
-
-function copyWithLineNumbers(filePath?: string) {
-  const str = (filePath ? `File: ${filePath}${EOL}` : '') + getLinesWithNumbers()
-  copy(str)
-  vscode.window.showInformationMessage('Copied!')
-}
-
-const commands = {
-  [COMMAND_IDS.WITHOUT_PATH]: () => {
-    copyWithLineNumbers()
-  },
-  [COMMAND_IDS.WITH_FULL_PATH]: () => {
-    const fullPath = vscode.window.activeTextEditor.document.fileName
-    copyWithLineNumbers(fullPath)
-  },
-  [COMMAND_IDS.WITH_FILE_NAME]: () => {
-    const fullPath = vscode.window.activeTextEditor.document.fileName
-    const fileName = path.basename(fullPath)
-    copyWithLineNumbers(fileName)
-  },
-}
-
-export function runCommand(commandId) {
+function copyWithLineNumbers(option?) {
   const editor = vscode.window.activeTextEditor
   if (!editor) {
     vscode.window.showInformationMessage('No editor is active')
     return
   }
 
-  const command = commands[commandId]
-  if (command) command()
+  const document = editor.document
+  const fullPath = document.fileName
+  const fileName = path.basename(fullPath)
+
+  let file = ''
+  switch (option) {
+    case OPTION_WITH_FULL_PATH:
+      file = fullPath
+      break
+    case OPTION_WITH_FILE_NAME:
+      file = fileName
+      break
+    default:
+      break
+  }
+
+  let str = file ? `File: ${file}${EOL}` : ''
+
+  const selections = [...editor.selections].sort((a, b) => a.start.line - b.start.line)
+  const lastSelection = selections[selections.length - 1]
+  const largestLineNumber = lastSelection.end.line + 1
+  const largestLineNumberLength = largestLineNumber.toString().length
+
+  selections.forEach((selection, i) => {
+    if (i > 0) str += `${MULTI_SELECTION_SEPARATOR}${EOL}`
+
+    for (let n = selection.start.line; n <= selection.end.line; n += 1) {
+      const number = leftPad(n + 1, largestLineNumberLength, 0)
+      const line = document.lineAt(n).text
+
+      str += `${number}: ${line}${EOL}`
+    }
+  })
+
+  copy(str)
+  vscode.window.showInformationMessage('Copied!')
+}
+
+export const commands = {
+  [COPY_WITH_LINE_NUMBERS]: () => {
+    copyWithLineNumbers()
+  },
+  [COPY_WITH_LINE_NUMBERS_WITH_FULL_PATH]: () => {
+    copyWithLineNumbers(OPTION_WITH_FULL_PATH)
+  },
+  [COPY_WITH_LINE_NUMBERS_WITH_FILE_NAME]: () => {
+    copyWithLineNumbers(OPTION_WITH_FILE_NAME)
+  },
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
 'use strict'
 
 import * as vscode from 'vscode'
-import { COMMAND_IDS, runCommand } from './copy-with-line-numbers'
+import { commands } from './copy-with-line-numbers'
 
 export function activate(context: vscode.ExtensionContext) {
   const { registerCommand } = vscode.commands
   const { subscriptions } = context
 
-  Object.keys(COMMAND_IDS).forEach((key) => {
-    const commandId = COMMAND_IDS[key]
-    subscriptions.push(registerCommand(commandId, () => { runCommand(commandId) }))
+  Object.keys(commands).forEach((key) => {
+    const command = commands[key]
+    subscriptions.push(registerCommand(key, () => { command() }))
   })
 }
 
